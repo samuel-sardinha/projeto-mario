5b7739c74a6a3d0c48da394b7e8ec4573fe35490
samuel-sardinha <123114910+samuel-sardinha@users.noreply.github.com>
2023-01-22 15:07:35 -0300

    Update index.js

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,8 +1,9 @@
 const botaoTrailer = document.querySelector('.botao-trailer');
-const fechaModal = document.querySelector(".fechar-modal");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
 const modal = document.querySelector(".modal");
 const linkDoVideo = video.src;
-const video = document.getElementById("video");
+
 
 function alternarmodal(){
     modal.classList.toggle("aberto");
